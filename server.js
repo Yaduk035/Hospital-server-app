74b7fb91ceed5db66bc411f67b9d4988a2d2e6e4
Yaduk035 <122448090+Yaduk035@users.noreply.github.com>
2023-04-22 20:05:43 +0530

    4th commit

--- server.js
+++ server.js
@@ -1,50 +1,82 @@
-const express = require("express");
-const fs = require("fs");
+import express from "express";
+import fs from "fs";
+
 const app = express();
-
-const PORT = process.env.PORT || 1000;
-
-let hospitalsData = [];
+const PORT = 3000;
 
 app.use(express.json());
 
-app.get("/hospitals", (req, res) => {
-  hospitalsData = JSON.parse(fs.readFileSync("./hospitals.json"));
-  res.json(hospitalsData);
+
+const rawData = fs.readFileSync("./hospitaldata.json");
+
+//Available Routes
+app.get("/", (req, res) => {
+  const routes = [
+    { GET: "/all/hospital" },
+    { POST: "/add/hospital" },
+    { PUT: "/update/hospital" },
+    { DELETE: "/delete/hospital" },
+  ];
+
+  res.json(routes);
 });
 
-app.post("/hospitals", (req, res) => {
-  hospitalsData.push(req.body);
-  fs.writeFileSync("./hospitals.json", JSON.stringify(hospitalsData));
-  res.json({ message: "Hospital added successfully!" });
+
+
+//Get
+app.get("/all/hospital/:id", (req, res) => {
+  const recordID = Number(req.params.id);
+  const data = JSON.parse(rawData);
+
+  const resultData = data.filter((each) => each.id === recordID);
+  res.send(resultData);
 });
 
-app.put("/hospitals/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  hospitalsData = JSON.parse(fs.readFileSync("./hospitals.json"));
-  const hospitalToUpdate = hospitalsData.find((hospital) => hospital.id === id);
-  if (!hospitalToUpdate) {
-    res.status(404).json({ message: "Hospital not found" });
-  } else {
-    hospitalToUpdate.name = req.body.name;
-    hospitalToUpdate.patients = req.body.patients;
-    hospitalToUpdate.place = req.body.place;
-    fs.writeFileSync("./hospitals.json", JSON.stringify(hospitalsData));
-    res.json({ message: "Hospital updated successfully!" });
-  }
+
+
+//Post
+app.post("/add/hospital/:id", (req, res) => {
+  const postData = req.body;
+  const currentData = JSON.parse(rawData);
+  currentData.push(postData);
+
+  const updatedData = JSON.stringify(currentData);
+  fs.writeFileSync("./hospitaldata.json", updatedData);
+
+  res.send("Data Updated Successfully");
 });
 
-app.delete("/hospitals/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  hospitalsData = JSON.parse(fs.readFileSync("./hospitals.json"));
-  const filteredData = hospitalsData.filter((hospital) => hospital.id !== id);
-  if (filteredData.length === hospitalsData.length) {
-    res.status(404).json({ message: "Hospital not found" });
-  } else {
-    hospitalsData = filteredData;
-    fs.writeFileSync("./hospitals.json", JSON.stringify(hospitalsData));
-    res.json({ message: "Hospital deleted successfully!" });
-  }
+
+//Put
+app.put('/update/hospital',(req,res) => {
+    const postData = req.body;
+    const data = JSON.parse(rawData);
+    const currentdata = data.find(each => each.id === Number(postData['id']));
+    const updatedData = {...currentdata,...postData};
+    const updatedArray = data.map(each => {
+        if(each.id === Number(postData['id'])){
+            return updatedData;
+        }
+        return each;
+    })
+    console.log(updatedArray);
+    fs.writeFileSync('./hospitaldata.json',JSON.stringify(updatedArray));
+    res.send("Updated Successfully");
+})
+
+
+//Delete
+app.delete('/delete/hospital/:id',(req,res) => {
+    const recordID = req.params.id;
+    const data = JSON.parse(rawData);
+    const updatedArray = data.filter(each => each.id !== Number(recordID));
+    fs.writeFileSync('./hospitaldata.json', JSON.stringify(updatedArray));
+    res.send(`Hospital with ID ${recordID} deleted successfully`);
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`app listetning at ${PORT}`);
 });
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
